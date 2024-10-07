d9d4629b5462e19776f7fc82ce150c1a89120ad8
Jhonn.SR <jrodriguez@corvatech.com>
2024-10-06 22:15:56 -0500

    Se implementa opcion de descarga

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, send_file
 from flask_socketio import SocketIO, emit
 from components.image_servidor import ImagenServidor
+import os
+import requests
 
 app = Flask(__name__)
 socketio = SocketIO(app)
@@ -8,6 +10,8 @@ socketio = SocketIO(app)
 # Inicializar el servicio de imágenes con tu API Key de Pixabay
 API_KEY = "18692132-8999bfe8bc634497a86b29f30"  # Reemplaza esto con tu API Key real
 image_service = ImagenServidor(api_key=API_KEY)
+
+IMAGE_FOLDER = 'downloads'
 
 @app.route('/')
 def index():
@@ -20,6 +24,29 @@ def handle_request_image(object_name):
         emit('send_image', {'image_path': image_url})
     else:
         emit('send_image', {'image_path': None})
+        
+@app.route('/download_image/<path:image_url>/<string:custom_filename>', methods=['GET'])
+def download_image(image_url, custom_filename):
+    print(image_url)
+    image_filename = os.path.basename(custom_filename)  # Obtiene el nombre del archivo de la URL
+    # image_path = os.path.join(IMAGE_FOLDER, image_filename)
+    image_path = os.path.join(IMAGE_FOLDER)
+
+    if os.path.exists(image_path):
+        os.remove(image_path)
+    
+    # Descargar la imagen solo si no existe localmente
+    if not os.path.exists(image_path):
+        try:
+            response = requests.get(image_url)
+            response.raise_for_status()  # Lanza un error si la solicitud falla
+            with open(image_path, 'wb') as f:
+                f.write(response.content)  # Guarda el contenido de la imagen en el archivo
+        except requests.RequestException as e:
+            return f"Error al descargar la imagen: {str(e)}", 500
+
+    # Envía el archivo para su descarga
+    return send_file(image_path, as_attachment=True, download_name=image_filename+'.jpg')
 
 if __name__ == '__main__':
     socketio.run(app, debug=True)
